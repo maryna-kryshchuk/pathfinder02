f4614b19587a7a33afd2392b9fb9553ccb5e5c9e
Maryna Kryshchuk <maryna.kryshchuk@gmail.com>
2020-01-11 20:26:17 +0200

    Add files via upload

--- pathfinder/libmx/src/mx_memcmp.c
+++ pathfinder/libmx/src/mx_memcmp.c
@@ -2,6 +2,7 @@
 
 int mx_memcmp(const void *s1, const void *s2, size_t n) {
     size_t i = 1;
+    
     while (*((unsigned char *)s1) == *((unsigned char *)s2) && i < n) {
         if (*((unsigned char *)s1) == '\0' || *((unsigned char *)s2) == '\0')
             return *((unsigned char *)s1) - *((unsigned char *)s2);
